1e822b37359a900d3d09f037c66b6cf78ad9cfd6
HamonBoi <128698282+HamonBoi@users.noreply.github.com>
2024-03-04 17:16:11 +0100

    Update expGMPY2.py

--- Code/expGMPY2.py
+++ Code/expGMPY2.py
@@ -1,20 +1,23 @@
-from gmpy2 import mpz, mpfr, mul, div, add, sub
+import gmpy2 as g
+
 
 def exp(x, n=50, precision = 53):
-    n = mpz(n)
+    g.get_context().precision = precision
+    n = g.mpz(n)
     if x < 0:
         l = 1
-        x = mpfr(-x, precision)
+        x = g.mpfr(-x)
     else:
         l = 0
-        x = mpfr(x, precision)
-    m = mpz(0)
+        x = g.mpfr(x)
+    m = g.mpz(0)
     while x > 1:
-        x = div(x, 2)
-        m = add(m, 1)
-    sum = mpfr(add(1, div(x, n)), precision)
+        x = g.div(x, g.mpfr(2))
+        m = g.add(m, 1)
+    sum = g.add(g.mpfr(1), g.div(x, n))
     for i in range(n, 0, -1):
-        sum = mpfr(add(1, mpfr(mul(div(x, i), sum), precision)), precision)
+        sum = g.mul(sum, g.div(x, g.mpfr(i)))
+        sum = g.add(g.mpfr(1), sum)
     for i in range(m):
-        sum = mpfr(mul(sum, sum), precision)
+        sum = g.mul(sum, sum)
     return sum
